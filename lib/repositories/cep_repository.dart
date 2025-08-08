17e4e6dcda61e4ed978ec952565282dbda9daec2
Leandro <l.silva01@uol.com.br>
2025-08-08 21:19:29 +0100

    criação da camada de dados

---
+++ lib/repositories/cep_repository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:busca_cep_app/models/cep_model.dart';
+import 'package:http/http.dart' as http;
+
+class CepRepository {
+  static const String _baseUrl = 'https://viacep.com.br/ws';
+  final http.Client client;
+
+  CepRepository({required this.client});
+
+  Future<CepModel> consultarCep(String cep) async {
+    final cleanCep = cep.replaceAll(r'[^0-9]', '');
+    if (cleanCep.length != 8) {
+      throw Exception('CEP deve conter exatamente 8 dígitos');
+    }
+
+    final url = Uri.parse('$_baseUrl/$cleanCep/json/');
+    try {
+      final response = await client.get(url);
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        if (jsonData.containKey('erro')) {
+          throw Exception('CEP não encontrado');
+        }
+        return CepModel.fromJson(jsonData);
+      } else {
+        throw Exception('Erro na requisição');
+      }
+    } on Exception catch (e) {
+      throw Exception('Erro ao consultar CEP: ${e.toString()}');
+    }
+  }
+}
